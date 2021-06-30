753b0a53921450cbac36bbd0b75cfd552af34627
Xavier Paredes <jxavierpp1@gmail.com>
2021-06-29 17:42:36 -0700

    📝 Update Contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/jxavierpp',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/xwalls',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://www.linkedin.com/in/jxavierpp/',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:jxavierpp1@gmail.com?subject=Hi',
   },
 ]
 
@@ -44,8 +44,8 @@ export default function Contact() {
         <h1 style={{ marginTop: 0 }}>Contact</h1>
 
         <div className={contactStyles.name}>
-          JJ Kasper - Next.js Engineer @{' '}
-          <ExtLink href="https://vercel.com">Vercel</ExtLink>
+          Xavier Paredes - Computer Scientist @{' '}
+          <ExtLink href="https://github.com/IslasGECI/">IslasGECI</ExtLink>
         </div>
 
         <div className={contactStyles.links}>
