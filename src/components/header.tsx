3a566e3bedfdf809d3b01319c852955dcdfc7888
Xavier Paredes <jxavierpp1@gmail.com>
2021-06-29 17:43:02 -0700

    📝 Update Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  { label: 'Github', link: 'https://github.com/xwalls' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -19,14 +19,14 @@ const Header = ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} My Personal Blog</title>
         <meta
           name="description"
-          content="An example Next.js site using Notion for the blog"
+          content="Learn. Share Knowladge. Receive Feedback. Repeat."
         />
-        <meta name="og:title" content="My Notion Blog" />
+        <meta name="og:title" content="My Personal Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="@jxavierpp" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
